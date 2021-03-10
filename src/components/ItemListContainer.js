dbf1b13a53c759316d2e0af94cd4762507208ca3
Julian Denaro <juliandenaro@gmail.com>
2021-03-10 18:42:00 -0300

    Editado ItemListContainer.js

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,14 +4,28 @@ export const ItemListContainer = () => {
     return (
         <div className="container">
             <div className="row mt-5">
-                <div className="col-8">
-                    <p>Soy una columna! Aca van a estar algunos productos..</p>
-                    <div className="row">
-                        
+                <div className="col-8">                
+                    <div className="card flex-row flex-wrap p-3">
+                        <div className="card-header border-0">
+                            <img src="//placehold.it/200" alt="" />
+                        </div>
+                        <div className="card-block px-2">
+                            <h4 className="card-title">Titulo producto</h4>
+                            <p className="card-text">Descripcion</p>
+                            <a href="#" className="btn btn-primary">Boton</a>
+                        </div>
+
                     </div>
+
                 </div>
-                <div className="col-4">
-                    <p>Soy otra columna! Aca van a estar tus items del carrito..</p>
+                <div className="col-4">                    
+                    <div className="card border-secondary mb-3">
+                    <div className="card-header">Carrito</div>
+                    <div className="card-body text-secondary">
+                        <h5 className="card-title">Nombre item</h5>
+                        <p className="card-text">Cantidad:1</p>
+                    </div>
+                    </div>
                 </div>
             </div>    
         </div>
