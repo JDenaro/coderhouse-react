07019acf8f5bd1137676634818b389b12244f5ce
Julian Denaro <juliandenaro@gmail.com>
2021-03-17 18:30:37 -0300

    Se agregaron ItemDetailContainer e ItemDetail

--- src/ReactiveWallet.js
+++ src/ReactiveWallet.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { ItemListContainer } from './components/ItemListContainer'
-import { NavBar } from './components/NavBar'
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
+import { NavBar } from './components/NavBar/NavBar'
 
 export const ReactiveWallet = () => {
     return (
