0a834b90d743af64040ed1caddc7929793b84619
Julian Denaro <juliandenaro@gmail.com>
2021-03-27 14:51:08 -0300

    Cambios de archivos de data

---
+++ src/assets/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        "id": 1,
+        "title": "Trezor Model T",
+        "manufacturer": "Trezor",
+        "price": 177.56,
+        "pictureUrl": "https://shop.trezor.io/static/img/product/T2.jpg",
+        "description": "The Trezor Model T is an advanced cryptocurrency hardware wallet. Store Bitcoin, passwords, tokens and other keys with confidence. Invented for your digital independence.",
+        "category": "Hardware Wallet"
+    },
+    {
+        "id": 2,
+        "title": "Trezor One Black",
+        "manufacturer": "Trezor",
+        "price": 58.10,
+        "pictureUrl": "https://shop.trezor.io/static/img/product/T1.png",
+        "description": "The Trezor Model One is the most secure hardware wallet choice for everyone, whether you're new to Bitcoin or already an expert. Get yours today!",
+        "category": "Hardware Wallet"
+    },
+    {
+        "id": 3,
+        "title": "Trezor One White",
+        "manufacturer": "Trezor",
+        "price": 58.10,
+        "pictureUrl": "https://shop.trezor.io/static/img/product/T1_white.png",
+        "description": "The Trezor Model One is the most secure hardware wallet choice for everyone, whether you're new to Bitcoin or already an expert. Get yours today!",
+        "category": "Hardware Wallet"
+    },
+    {
+        "id": 4,
+        "title": "Trezor One Metallic",
+        "manufacturer": "Trezor",
+        "price": 595.80,
+        "pictureUrl": "https://shop.trezor.io/static/img/product/metallic_front.png",
+        "description": "An exclusive Trezor Model One device handmade from anodized aluminum, with an engraving commemorating the 5th anniversary of SatoshiLabs, the makers of Trezor.",
+        "category": "Hardware Wallet"
+    },
+]
