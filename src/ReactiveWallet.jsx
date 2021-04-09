f844aaae13af1e03996507987afa9c46b9712594
Julian Denaro <juliandenaro@gmail.com>
2021-04-08 23:30:01 -0300

    AddItem detecta si se agrega articulo repetido

--- src/ReactiveWallet.jsx
+++ src/ReactiveWallet.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react'
 import { RouterApp } from './routes/RouterApp';
+import CartContext from './context/CartContext'
 
 export const ReactiveWallet = () => {
 
   const [cart, setCart] = useState([])
+
+  const addItem = (id, qty) => {
+    console.log('se recibe item id:', id, ' con cantidad: ', qty)
+    const repeated = cart.some(item => item.id === id)
+    console.log('repeated:', repeated)
+    repeated
+      ? setCart(cart.map(item => { if (item.id === id) { item.id = item.id + qty } }))
+      : setCart([...cart, { id, qty }])
+  }
+
+  const clearCart = () => setCart([])
+
   return (
     <>
-      <RouterApp />
+      <CartContext.Provider value={{ cart: cart, addItem: addItem, clearCart: clearCart }}>
+        <RouterApp />
+      </CartContext.Provider>
     </>
   )
 }
