f9fce889b957c04f1e8d262e84621a82a2e36a14
Julian Denaro <juliandenaro@gmail.com>
2021-04-12 08:03:45 -0300

    ItemDetailContainer ahora hace la consulta a firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router';
 import { products } from '../../assets/data/products';
+import { getFirestore } from '../../configs/firebase';
 import { ItemDetail } from '../ItemDetail/ItemDetail';
 import { ItemSpinner } from '../ItemSpinner/ItemSpinner';
 
@@ -8,19 +9,43 @@ export const ItemDetailContainer = () => {
 
     const { id } = useParams()
     // console.log(id)
-    const [items, setItems] = useState([]);
+
+    // const [items, setItems] = useState([]);
+    // useEffect(() => {
+    //     new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve(products);
+    //         }, 1100);
+    //     }).then((resultado) => setItems(resultado.filter(item => item.id === parseInt(id))));
+    // }, [id]);
+
+
+    const [itemDetailed, setItemDetailed] = useState([]);
+
+    // firebase
     useEffect(() => {
-        new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(products);
-            }, 1100);
-        }).then((resultado) => setItems(resultado.filter(item => item.id === parseInt(id))));
+        const db = getFirestore();
+        const categoriasCollection = db.collection("items");
+
+        categoriasCollection
+            .get()
+            .then((resp) => {
+                if (resp.size === 0) {
+                    console.log("Sin datos");
+                } else {
+                    let arr = []
+                    resp.docs.map((c) => arr.push({ id: parseInt(c.id), ...c.data() }));
+                    setItemDetailed(arr.filter(item => item.id === parseInt(id)))
+                    console.log(itemDetailed)
+                }
+            })
+            .catch((error) => console.log(error));
     }, [id]);
 
     return (
         <div className="container mt-5">
             {
-                items.length < 1 ? <ItemSpinner /> : <ItemDetail items={items[0]} />
+                itemDetailed.length < 1 ? <ItemSpinner /> : <ItemDetail items={itemDetailed[0]} />
             }
         </div>
     )
